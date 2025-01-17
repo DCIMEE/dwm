ee641da69f46cf72e8b7f0da207fb6413689b05e
DCIMEE <backslash74@outlook.com>
2025-01-17 23:46:29 +1300

    audioctr_upd

--- config.def.h
+++ config.def.h
@@ -47,15 +47,15 @@ typedef struct {
 };
 
 /* audio control ALSA */
-/*static const char *mutecmd[] = { "amixer", "-q", "set", "Master", "toggle", NULL };
+static const char *mutecmd[] = { "amixer", "-q", "set", "Master", "toggle", NULL };
 static const char *volupcmd[] = { "amixer", "-q", "set", "Master", "5%+", "unmute", NULL };
 static const char *voldowncmd[] = { "amixer", "-q", "set", "Master", "5%-", "unmute", NULL };
-static const char *miccmd[] = { "amixer", "set", "Capture", "toggle", NULL };*/
+static const char *miccmd[] = { "amixer", "set", "Capture", "toggle", NULL };
 
 /* audio control by pactl */
-static const char *mutecmd[] = { "pactl", "set-sink-mute",   "0", "toggle",  NULL };
+/* static const char *mutecmd[] = { "pactl", "set-sink-mute",   "0", "toggle",  NULL };
 static const char *volupcmd[]   = { "pactl", "set-sink-volume", "0", "+5%",     NULL };
-static const char *voldowncmd[] = { "pactl", "set-sink-volume", "0", "-5%",     NULL };
+static const char *voldowncmd[] = { "pactl", "set-sink-volume", "0", "-5%",     NULL }; */
 
 /* backlight*/
 static const char *brupcmd[] = { "sudo", "xbacklight", "-inc", "10", NULL };
